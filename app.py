22b2619ea2659e1317c822c82d865bb37a7113c7
famabarham <sfama@ept.sn>
2023-07-11 01:51:20 +0000

    first-commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+from flask_restful import Api, Resource,request
+import pickle
+import json
+from model import generate_Cloud,comptage
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/worldCLoud', methods=['POST'])
+def prepare_worldCloud():
+    data = request.get_json()['reponses']
+    datas = generate_Cloud(data)
+    return datas
+
+@app.route('/sentimentAnalysis', methods=['POST'])
+def prepare_sentimentAnalysis():
+    data = request.get_json()['reponses']
+    datas = comptage(data)
+    return datas
+
+
+# if __name__ == "__main__":
+#     app.run()
+
+
+
+
+
---
+++ model.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify, request 
+from wordcloud import WordCloud
+from transformers import pipeline
+from googletrans import Translator, constants
+from pprint import pprint
+
+
+sentiment_pipeline = pipeline(model="j-hartmann/sentiment-roberta-large-english-3-classes")
+# init the Google API translator
+translator = Translator()
+
+def comptage(data):
+    nliste=0
+    pliste=0
+    neuliste=0
+    liste = sentiment_pipeline(translate_to_eng(data.split('$')))
+    for i in liste:
+        if (i['label'] == 'negative'):
+            nliste=nliste+1
+        elif (i['label'] == 'positive'):
+            pliste=pliste+1
+        elif (i['label'] == 'neutral'):
+            neuliste= neuliste+1
+    data={'positive':pliste,'negative':nliste, 'neutral': neuliste}
+    return data
+
+def generate_Cloud(data):
+    word_cloud = WordCloud(collocations = False, background_color = 'white', max_words = 10, max_font_size = 200).generate(data)
+    # Display the generated Word Cloud
+    return WordCloud().process_text(data)
+
+def translate_to_eng(liste):
+    liste2=[]
+    translations = translator.translate(liste, dest="en",src="fr")
+    for translation in translations:
+        liste2=liste2+[translation.text]
+    return liste2
+
+
